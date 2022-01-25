8cfaeb98948c54038571cc35f51aa2a00cf7c7e8
Mehedi Hasan Maruf <41261534+MjMehediHasan@users.noreply.github.com>
2022-01-26 01:31:22 +0600

    Welcome to my home !🤪

--- welcome.py
+++ welcome.py
@@ -5,10 +5,10 @@ def loop(r, c):
         reAfter = int(c-3*(2*(r-i)-1))      # Doing (r-i) for reverseing the same upper thing
         remWelcome = int(c-7)               # Welcomes constant length is 7. That's why
         if (i < rwMid):
-            print("-"*(reBefore//2) + ".|."*(2*i+1) + "-"*(reBefore//2))
+            print("-"*(reBefore//2) + "•|•"*(2*i+1) + "-"*(reBefore//2))
         elif (i == rwMid):
             print("-"*(remWelcome//2) + "WELCOME" + "-"*(remWelcome//2))
         elif (i > rwMid):
-            print("-"*(reAfter//2) + ".|."*(2*(r-i)-1) + "-"*(reAfter//2))  # Doing -1 because (r-i) will not go till 0 it will go till 1
+            print("-"*(reAfter//2) + "•|•"*(2*(r-i)-1) + "-"*(reAfter//2))  # Doing -1 because (r-i) will not go till 0 it will go till 1
 inputs = int(input("Rows : "))
 loop(inputs, inputs*3)
